d84ca7bb1f4c6da9e376446b78e6b56024217c3e
ArGxento <argxento@gmail.com>
2013-12-13 18:25:27 +0900

    add files

---
+++ src/parser/parser.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+#include <boost/spirit/include/qi.hpp>
+
---
+++ src/parser/paser.cpp
@@ -0,0 +1,25 @@
+#include "paser.hpp"
+
+
+template <typename itrT>
+struct Grammar :qi::grammar<itrT> {
+Grammar:Grammer::base_type(op2) {
+	using qi::omit; using qi::lit; using qi::raw;
+
+	dec_ltr = omit[
+		qi::int_[
+			[&](int i, unused_type, unused_type){
+				this->acmStk.push(i);
+				return;
+			}
+		]
+	];
+	ltr = dec_ltr;
+	
+	ltr % "**" % (lit('*') | '/' | '%') | ()
+
+		};
+
+			qi::rule<itrT> dec_ltr, ltr, op1, op2;
+};
+
